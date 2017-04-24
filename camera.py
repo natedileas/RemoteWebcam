4f6526e7e19628c831eb1add26691f5ad9ca64ae
Nathan Dileas <ndileas@gmail.com>
2017-04-24 16:30:08 -0400

    Structural change: changed calling syntax to be more user-friendly.
    Now you can use a RemoteCamera instance like you would a
    cv2.VideoCapture instance.

--- camera.py
+++ camera.py
@@ -1,41 +1,125 @@
 import cv2
+import numpy
+
 from server import ImageSocket
-import time
 
 
-def send_images(hostname, port, framerate=30, encoding=None):
-    interval = 1/float(framerate)
+class RemoteCamera(object):
+    """ Emulate a cv2.VideoCapture Interface over a network.
 
-    sock = ImageSocket()
-    sock.connect((hostname, port))
+    As much as possible, this class follows the class documentation here:
+    http://docs.opencv.org/2.4/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imwrite#videocapture-videocapture
 
-    cam = cv2.VideoCapture(0)
+    A couple exceptions:
+     - RuntimeErrors may occur as part of normal operation. They mean your
+       connection to the machine with the webcam has broken.
+     - You can set the quality of the stream with set_jpeg_quality(int)
 
-    try:
-        while True:
-            start = time.time()
-            flag, frame = cam.read()
+    An example use of this class is available in the test harness in this file.
 
-            if not flag:
-                print('No image was read')
-                break
+    Author: Nathan Dileas, 2017, ndileas@gmail.com
+    No warranty, no license, just reference me.
+    """
+    def __init__(self, camId, hostname, port):
+        # set up camera on other machine
+        self.connection = ImageSocket()
+        self.connection.connect((hostname, port))
+        self.connection.csend(camId)
 
-            sock.csend(frame)
+    def read(self):
+        self.connection.csend('read')
+        flag, eframe = self.connection.crecieve()
 
-            elapsed = start - time.time()
-            if elapsed < interval:
-                time.sleep(round(interval - elapsed - 0.0001, 5))
+        frame = cv2.imdecode(eframe, 1)
 
-    except KeyboardInterrupt:
-        print('Exiting')
-    except ConnectionResetError: 
-        print ('Connection Reset')
-    finally:
-        cam.release()
-        sock.close()
+        return flag, frame
+
+    def open(self):
+        """ open camera for reading """
+        self.connection.csend('open')
+        retval = self.connection.crecieve()
+
+        return retval
+
+    def isOpened(self):
+        """ return true if opened """
+        self.connection.csend('isOpened')
+        retval = self.connection.crecieve()
+
+        return retval
+
+    def release(self):
+        """ release the resources associated with the camera """
+        self.connection.csend('release')
+
+    def grab(self):
+        """ The methods/functions grab the next frame from video file or camera 
+        and return true (non-zero) in the case of success """
+        self.connection.csend('grab')
+        retval = self.connection.crecieve()
+
+        return retval
+
+    def retrieve(self):
+        """ decode and return the just grabbed frame. If no frames has been 
+        grabbed (camera has been disconnected, or there are no more frames in 
+        video file), the methods return false and the functions return NULL 
+        pointer. """
+        self.connection.csend('retrieve')
+        retval, image = self.connection.crecieve()
+
+        return image
+
+    def get(self, prop):
+        self.connection.csend(('get', prop))
+        value = self.connection.crecieve()
+
+        return value
+
+    def set(self, prop, newval):
+        self.connection.csend(('set', prop, newval))
+        flag = self.connection.crecieve()
+
+        return value
+
+    def set_jpeg_quality(self, newval, *args):
+        self.connection.csend(('set_jpeg_quality', newval))
+
 
 if __name__ == '__main__':
-    HOSTNAME = '129.21.57.78'
-    PORT = 12456
+    # if this example does not work for you, try:
+    # disabling your firewall
+    # changing the port #
+    # double checking hostname spelling
+
+    import time
+
+    HOSTNAME = '129.21.52.194'   # must be changed to machine with webcam IP address or hostname
+    PORT = 12456    # int, > 4 digits, < 65555
     
-    send_images(HOSTNAME, PORT)
+    cam = RemoteCamera(0, HOSTNAME, PORT)   # create instance
+
+    window = cv2.namedWindow('winname')
+    slider = cv2.createTrackbar('JPEG Quality', 'winname', 75, 100, cam.set_jpeg_quality)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    t = 0
+
+    while True:
+        s = time.time()
+
+        flag, frame = cam.read()
+        if not flag: break
+        
+        cv2.putText(frame, 'Latency: '+str(t), (10,frame.shape[0]-10), font, 0.5, (0, 0, 255))
+
+        cv2.imshow('winname', frame)
+        key = cv2.waitKey(10)
+
+        t = round((time.time() - s) * 1000.)
+
+        if key == -1: 
+            continue
+        elif chr(key) == 'q' or 'Q':
+            break
+
+    cam.release()  # explicit is necesary with current implmentation
--- server.py
+++ server.py
@@ -3,8 +3,17 @@ import threading
 import queue
 import pickle
 
+import numpy
+import cv2
+
 class ImageSocket(socket.socket):
-    MSG_LEN = 10
+    """ Provide a simple tcp socket for the transfer of python objects
+
+    Author: Nathan Dileas, 2017, ndileas@gmail.com
+    No warranty, no license, just reference me.
+    """
+
+    MSG_LEN = 8
     CHUNK = 2048
 
     def __init__(self):
@@ -17,6 +26,12 @@ class ImageSocket(socket.socket):
         return _recieve(self)
 
 def _send(sock, obj):
+    """ Send a message with pickle serialization. 
+
+    Args:
+        sock: open socket to send data on
+
+    """
     totalsent = 0
     msg = pickle.dumps(obj)
     msglen = str(len(msg)).zfill(ImageSocket.MSG_LEN).encode()
@@ -30,6 +45,17 @@ def _send(sock, obj):
         totalsent += sent
 
 def _recieve(sock):
+    """ Recieve a message with pickle serialization. 
+
+    Args:
+        sock: open socket to receive data on
+
+    Returns:
+        stuff: pickle deserialized message
+
+    Raises:
+        RuntimeError: if no data to be received on socket.
+    """
     msglen = sock.recv(ImageSocket.MSG_LEN).decode()
 
     if len(msglen) == 0:
@@ -52,41 +78,94 @@ def _recieve(sock):
         raise RuntimeError('no data recv\'d')
 
     stuff = pickle.loads(data)
-
+    
     return stuff
 
-class ServerThread(threading.Thread):
-    def __init__(self, sock, queue):
-        super(ServerThread, self).__init__()
+
+class RemoteCamThread(threading.Thread):
+    """ Act as the backend to RemoteCamera (serverside)
+
+    An example use of this class is available in the test harness in this file.
+
+    Author: Nathan Dileas, 2017, ndileas@gmail.com
+    No warranty, no license, just reference me.
+    """
+    def __init__(self, sock):
+        super(RemoteCamThread, self).__init__()
         self.sock = sock
-        self.queue = queue
         self.serve = threading.Event()
+        self.jpg_quality = 75
 
     def join(self, timeout=None):
         self.serve.set()
-        super(ServerThread, self).join(timeout)
+        super(RemoteCamThread, self).join(timeout)
 
     def run(self):
-        while not self.serve.isSet():
-            try:
-                data = _recieve(self.sock)
-                self.queue.put(data)
+        camID = _recieve(self.sock)
+        self.camera = cv2.VideoCapture(camID)
 
-            except RuntimeError:
-                self.sock.close()
-                print('Connection Closed')
-                break
+        try:
+            while not self.serve.isSet():
+                
+                data = _recieve(self.sock)
+                response = None
+
+                if data == 'read':
+                    flag, frame = self.camera.read()
+                    eflag, eframe = cv2.imencode('.jpg', frame, \
+                        [int(cv2.IMWRITE_JPEG_QUALITY), self.jpg_quality])
+                    response = (flag, eframe)
+
+                elif data == 'open':
+                    retval = self.camera.open()
+                    response = retval
+
+                elif data == 'isOpened':
+                    retval = self.camera.isOpened()
+                    response = retval
+
+                elif data == 'grab':
+                    retval = self.camera.grab()
+                    response = retval
+
+                elif data == 'retrieve':
+                    flag, frame = self.camera.retrieve()
+                    eflag, eframe = cv2.imencode('.jpg', frame)
+                    response = (flag, eframe)
+
+                elif 'get' in data:
+                    value = self.camera.get(data[1])
+                    response = value
+
+                elif 'set' in data:
+                    retval = self.camera.set(data[1], data[2])
+                    response = retval
+
+                elif 'set_jpeg_quality' in data:
+                    self.jpg_quality = data[1]
+
+                elif data == 'release':
+                    self.camera.release()
+                    break
+
+                if response:
+                    _send(self.sock, response)
+
+        except RuntimeError:
+            print('RuntimeError: connection lost')
+        finally:
+            print('Connection Closed')
+            self.sock.close()
+            self.camera.release()
+
 
 class Server(threading.Thread):
+    """ Dispatch RemoteCamThreads. """
     def __init__(self, hostname, port):
         super(Server, self).__init__()
         self.hostname = hostname
         self.port = port
         self.serve = threading.Event()
-        self.queue = queue.Queue()
-        self.lock = threading.Lock()
-
-        self._state = {}
 
     def join(self, timeout=None):
         self.serve.set()
@@ -95,7 +174,7 @@ class Server(threading.Thread):
     def run(self):
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serversocket.bind((self.hostname, self.port))
-        serversocket.listen(2)   # 2 connection at a time
+        serversocket.listen(1)   # 1 queued connection at a time
         serversocket.settimeout(1)   # so self.serve.isSet can actually work
 
         while not self.serve.isSet():
@@ -104,13 +183,25 @@ class Server(threading.Thread):
                 (clientsocket, address) = serversocket.accept()
                 print('Connected')
                 clientsocket.setblocking(1)
-                st = ServerThread(clientsocket, self.queue)
+                st = RemoteCamThread(clientsocket)
                 st.start()
             except socket.timeout:
                 continue
 
 
 def setup(hostname, port):
+    """ Convienience func """
     server = Server(hostname, port)
     server.start()
     return server
+
+if __name__ == '__main__':
+    # if this example does not work for you, try:
+    # disabling your firewall
+    # changing the port #
+    # double checking hostname spelling
+    HOSTNAME = '129.21.52.194'  # must be changed to machine with webcam IP address or hostname
+    PORT = 12456    # int, > 4 digits, < 65555
+
+    s = Server(HOSTNAME, PORT)
+    s.run()   # not start() intentionally, keeps it alive
